c221c0b563fa71f6c4a1865df24dc9c58caa4482
rohithramalingamp-jpg <rohithramalingamp@gmail.com>
2026-06-16 18:36:31 +0530

    Remove: Delete invalid nitro.config.ts file

--- nitro.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineNitroConfig } from "nitropack/config";
-
-export default defineNitroConfig({
-  presets: ["node-server"],
-  output: {
-    dir: ".output",
-    serverDir: ".output/server"
-  }
-});
